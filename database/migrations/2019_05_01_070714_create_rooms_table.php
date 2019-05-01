3c0e1820d32e0a9d3ae3c4a9ace16e13371542bd
AbdumalikFront <iwillstudyingermany@gmail.com>
2019-05-01 12:22:42 +0500

    Models altered to match admin role

---
+++ database/migrations/2019_05_01_070714_create_rooms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->integer('room_number');
+			$table->integer('status');
+			$table->string('type');
+			$table->integer('max_adults');
+			$table->integer('max_children');
+			$table->integer('cost');
+        	$table->string('img_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/fake', 'HomeController@index')->name('home');
 
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
