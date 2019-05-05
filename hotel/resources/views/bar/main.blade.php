7ecae816b4fae9f27bdd36d122b6da5fb60cdb49
SaidaloST <33759392+SaidaloST@users.noreply.github.com>
2019-05-05 23:17:57 +0500

    changes for pixeles

--- hotel/resources/views/bar/main.blade.php
+++ hotel/resources/views/bar/main.blade.php
@@ -15,14 +15,14 @@
   {{-- Main --}}
   <section class="main slideanim">
     <div class="container pt-5">
-      <h1>Main Courses Menu</h1>
+      <h2>Main Courses Menu</h2>
       <div class="row">
           <div class="col-lg-6 p-5">
-              <h1 class="display-5">Plov&nbsp;<span>&#36;12</span></h1>
+              <h2 class="display-5">Plov&nbsp;<span>&#36;12</span></h2>
               <p>Plov is cooked with rice, fresh mutton or beef, yellow or red carrot, onions and vegetable oil. It is prepared in a deep cast-iron pot (kazan), which is uniformly warmed and a dish is not burnt.</p>
-              <h1 class="display-5">Chicken Parmagiana&nbsp;<span>&#36;10</span></h1>
+              <h2 class="display-5">Chicken Parmagiana&nbsp;<span>&#36;10</span></h2>
               <p>Chicken Surf & Turf $31 Char grilled garlic chicken breast fillet topped with a garlic seafood sauce & served with garden salad & steak fries or with steamed vegetables and potato.</p>
-              <h1 class="display-5">Gluten Free Surf & Turf&nbsp;<span>&#36;10</span></h1>
+              <h2 class="display-5">Gluten Free Surf & Turf&nbsp;<span>&#36;10</span></h2>
               <p>Char grilled Scotch fillet 300gm topped with a garlic seafood sauce & served with garden salad & steak fries or with steamed veggies roast chat potato.</p>
           </div>
           <div class="col-lg-6 overlay zoom p-5">
@@ -40,21 +40,21 @@
         <div class="col-lg-4 view overlay zoom p-5">
           <img src="images/dessert.jpeg" alt="">
           <div class="yoghurt pt-5">
-            <h1 class="display-5">Organic Yoghurt Panna Cotta&nbsp;<span>&#36;8</span></h1>
+            <h2 class="display-5">Organic Yoghurt Panna Cotta&nbsp;<span>&#36;8</span></h2>
             <p>Blackcurrant & limoncello compote, crunchy spiced crumble & lemon zest</p>
           </div>
         </div>
         <div class="col-lg-4 view overlay zoom p-5">
           <img src="images/bar/polette.jpeg" alt="">
           <div class="poleta pt-5">
-            <h1 class="display-5">Poleta Cake&nbsp;<span>&#36;10</span></h1>
+            <h2 class="display-5">Poleta Cake&nbsp;<span>&#36;10</span></h2>
             <p>Served with whipped crème fraîche, fresh orange & pomegranate</p>
           </div>
         </div>
         <div class="col-lg-4 view overlay zoom p-5">
           <img src="images/bar/vin-sano.jpg" alt="">
           <div class="vin-sano pt-5">
-            <h1 class="display-5">Vin Sano Tiramisu&nbsp;<span>&#36;10</span></h1>
+            <h2 class="display-5">Vin Sano Tiramisu&nbsp;<span>&#36;10</span></h2>
             <p>The classic Italian dessert with vin santo, chocolate shavings & orange zest</p>
           </div>
         </div>
@@ -65,17 +65,17 @@
   {{-- Drinks --}}
   <section class="drinks slideanim">
     <div class="container">
-      <h1 class="text-center">Drinks Menu</h1>
+      <h2 class="text-center">Drinks Menu</h2>
       <div class="row">
           <div class="col-lg-6 overlay zoom p-5">
               <img src="images/drink.jpeg" alt="" class="img-fluid">
           </div>
           <div class="col-lg-6 p-5">
-              <h1 class="display-5">Aperol Spritz&nbsp;<span>&#36;12</span></h1>
+              <h2 class="display-5">Aperol Spritz&nbsp;<span>&#36;12</span></h2>
               <p>A refreshing mix of Aperol, soda & Jamie’s Prosecco</p>
-              <h1 class="display-5">Elderflower Spritz&nbsp;<span>&#36;9</span></h1>
+              <h2 class="display-5">Elderflower Spritz&nbsp;<span>&#36;9</span></h2>
               <p>St Germain elderflower liqueur, Jamie’s Prosecco & soda</p>
-              <h1 class="display-5">Bellini&nbsp;<span>&#36;10</span></h1>
+              <h2 class="display-5">Bellini&nbsp;<span>&#36;10</span></h2>
               <p>White peach purée & Jamie’s Prosecco</p>
           </div>
       </div>
@@ -85,7 +85,7 @@
   {{-- Download --}}
   <section class="download slideanim">
     <div class="container text-center">
-      <h1 class="p-5">Download Full Menu</h1>
+      <h2 class="p-5">Download Full Menu</h2>
       <button type="submit" class="btn btn-primary pr-5 pl-5 mb-5"><i class="fa fa-download" aria-hidden="true"></i>&nbsp;Download now</button>
     </div>
   </section>
